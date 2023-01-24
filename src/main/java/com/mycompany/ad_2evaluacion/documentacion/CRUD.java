373fe3866d686c774a367f84591c35104207290e
Caio Medeiros <cmedeiros@sshteam.com>
2023-01-24 14:52:54 +0100

    Initial Commit

---
+++ src/main/java/com/mycompany/ad_2evaluacion/documentacion/CRUD.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ad_2evaluacion.documentacion;
+
+import java.util.Arrays;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.base.Resource;
+
+/**
+ *
+ * @author cmedeiros
+ */
+public class CRUD {
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException {
+        // Configuración de la base de datos
+        final String URI = "xmldb:exist://192.168.152.132:8080/exist/xmlrpc";
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // Inicialización del driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        // CREATE
+        // Conexión a la base de datos. 
+        // Primer parámetro: Ruta de la colección
+        // Segundo parámetro: Usuario
+        // Tercer parámetro: Contraseña para el usuario
+        Collection col = DatabaseManager.getCollection(URI + "/OperacionesCRUD", "admin", "admin");
+
+        // Creación del elemento XML
+        String xml = "<libro><autor>Gustavo</autor></libro>";
+
+        // Creación del objeto Resource con el XML
+        // Primer parámetro: Nombre del recurso que se quiere guardar
+        // Segundo parámetro: Tipo de recurso (XMLResource o BinaryResource)
+        Resource res = col.createResource("miLibro.xml", "XMLResource");
+        res.setContent(xml);
+
+        // Agregar el recurso a la colección
+        col.storeResource(res);
+
+
+        // READ
+        // Listar los recursos dentro de una colección
+        System.out.println(Arrays.toString(col.listResources()));
+        
+        // Ver contenido del recurso
+        System.out.println(col.getResource(col.listResources()[0]).getContent().toString());
+        
+        
+        // UPDATE
+        // Actualización de un recurso
+        String xml2 = "<libro><autor>Gustavo</autor></libro>";
+        Resource res2 = col.createResource("miLibro.xml", "XMLResource");
+        res.setContent(xml2);
+        col.storeResource(res2);
+
+
+        // DELETE
+        // Obtener el recurso que se quiera eliminar.
+        // Primer parámetro: ID del recurso
+        Resource eliminar = col.getResource("miLibro.xml");
+        
+        // Borrado
+        col.removeResource(eliminar);
+        
+    }
+}
---
+++ src/main/java/com/mycompany/ad_2evaluacion/tiposDeImplementacion/Consultas.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ad_2evaluacion.tiposDeImplementacion;
+
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.ResourceSet;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XQueryService;
+
+/**
+ *
+ * @author cmedeiros
+ */
+public class Consultas {
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, XMLDBException {
+        // Configuración de la base de datos
+        final String URI = "xmldb:exist://192.168.152.132:8080/exist/xmlrpc";
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // Inicialización del driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        // Obtener el servicio de XQuery
+        Collection col = DatabaseManager.getCollection(URI + "/OperacionesCRUD", "admin", "admin");
+        XQueryService service = (XQueryService) col.getService("XQueryService", "1.0");
+        service.setProperty("indent", "yes");
+
+        // Escribir la consulta en XQuery
+        String query = "for $book in /libros/libro where $book/titulo = 'Prueba Documentacion' return $book";
+
+        // Realizar la consulta
+        ResourceSet result = service.query(query);
+
+        // Comprobar los resultados
+        ResourceIterator i = result.getIterator();
+        while (i.hasMoreResources()) {
+            Resource r = i.nextResource();
+            System.out.println((String) r.getContent());
+        }
+    }
+}
